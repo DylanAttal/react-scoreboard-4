4749a23f1272b730cfc81a013d63d5012d36e777
Dylan Attal <attal.dylan@gmail.com>
2020-07-13 18:23:50 -0400

    css modules

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+export const Header = () => {
+  return (
+    <div>
+      <h1 className={styles.header}>React Scoreboard</h1>
+    </div>
+  )
+}
