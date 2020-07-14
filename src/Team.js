4be011cec59bf1e6318c9c5a0684a678565305ea
Dylan Attal <attal.dylan@gmail.com>
2020-07-14 19:44:59 -0400

    can update team name with hooks!

--- src/Team.js
+++ src/Team.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Team.module.css'
 
 export const Team = (props) => {
+  const [teamName, setTeamName] = useState('Gators')
+
+  let newTeamName = ''
+
+  const handleTeamNameInput = (event) => {
+    newTeamName = event.target.value
+  }
+
+  const handleTeamNameClick = () => {
+    setTeamName(newTeamName)
+  }
+
   return (
     <div>
-      <p className={styles.teamName}>Team Name</p>
+      <p className={styles.teamName}>{teamName}</p>
       <p>Team Score</p>
-      <input />
-      <button>Update team name</button>
+      <input onChange={handleTeamNameInput} />
+      <button onClick={handleTeamNameClick}>Update team name</button>
       <div className={styles.pointButtons}>
         <button>Subtract one</button>
         <button>Add one</button>
