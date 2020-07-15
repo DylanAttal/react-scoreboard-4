9fb6332272bf2edfca20da05d78212452e963757
Dylan Attal <attal.dylan@gmail.com>
2020-07-15 19:17:41 -0400

    can add and subtract points. clears input when team name updated

--- src/Team.js
+++ src/Team.js
@@ -3,26 +3,39 @@ import styles from './Team.module.css'
 
 export const Team = (props) => {
   const [teamName, setTeamName] = useState('Gators')
-
-  let newTeamName = ''
+  const [newTeamName, setNewTeamName] = useState('')
+  const [teamScore, setTeamScore] = useState(0)
 
   const handleTeamNameInput = (event) => {
-    newTeamName = event.target.value
+    setNewTeamName(event.target.value)
   }
 
   const handleTeamNameClick = () => {
     setTeamName(newTeamName)
+    setNewTeamName('')
+  }
+
+  const handleAddPoint = () => {
+    setTeamScore(teamScore + 1)
+  }
+
+  const handleSubtractPoint = () => {
+    if (teamScore === 0) {
+      return
+    }
+
+    setTeamScore(teamScore - 1)
   }
 
   return (
     <div>
       <p className={styles.teamName}>{teamName}</p>
-      <p>Team Score</p>
-      <input onChange={handleTeamNameInput} />
+      <p>{teamScore}</p>
+      <input onChange={handleTeamNameInput} value={newTeamName} />
       <button onClick={handleTeamNameClick}>Update team name</button>
       <div className={styles.pointButtons}>
-        <button>Subtract one</button>
-        <button>Add one</button>
+        <button onClick={handleSubtractPoint}>Subtract one</button>
+        <button onClick={handleAddPoint}>Add one</button>
       </div>
     </div>
   )
