ff014f8b91d670ce0df3f5f2b980f8343bcdcadd
Dylan Attal <attal.dylan@gmail.com>
2020-07-20 19:18:16 -0400

    Pass down teamname and teamscore as props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import { Header } from './Header'
 import { Team } from './Team'
 
 export function App() {
+  const listOfTeams = [
+    {
+      teamName: 'Gators',
+      teamScore: 0,
+    },
+    {
+      teamName: 'Seminoles',
+      teamScore: 0,
+    },
+    {
+      teamName: 'Dolphins',
+      teamScore: 0,
+    },
+    {
+      teamName: 'Bears',
+      teamScore: 0,
+    },
+  ]
+
   return (
     <div className='App'>
       <Header />
-      <Team />
+      {listOfTeams.map((team, index) => {
+        return (
+          <Team name={team.teamName} score={team.teamScore} index={index} />
+        )
+      })}
     </div>
   )
 }
--- src/Team.js
+++ src/Team.js
@@ -29,8 +29,8 @@ export const Team = (props) => {
 
   return (
     <div>
-      <p className={styles.teamName}>{teamName}</p>
-      <p>{teamScore}</p>
+      <p className={styles.teamName}>{props.name}</p>
+      <p>{props.score}</p>
       <input onChange={handleTeamNameInput} value={newTeamName} />
       <button onClick={handleTeamNameClick}>Update team name</button>
       <div className={styles.pointButtons}>
